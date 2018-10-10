1a052aead38b3696b2e22b145a7037819d747e7e
T. A. Marquard <talonmarquard@yahoo.com>
2018-10-10 13:14:38 -0600

    Add files via upload

--- rdysetgo.v
+++ rdysetgo.v
@@ -1,14 +1,22 @@
-module rdysetgo (A, B, C, D, blank, start, clk, reset);
+module rdysetgo (A, B, C, D, blank, correct, start, clk, reset);
 	output reg [3:0] A, B, C, D, blank;
 	input start, clk, reset;
+	input [1:0] correct;
 	reg [1:0] ctime;
+	reg ShowScore;
+	//reg [7:0] score, finalScore;
 	
 	always @ (posedge clk or posedge reset) begin  
-	       if (reset)
-	           ctime <= 0;	      
-	       else if (start)
-		       ctime <= ctime + 1;
-	       else ctime <= 2'b00;
+		if (reset)
+		begin
+			ctime <= 0;	
+			ShowScore <= 0;
+		end		
+	    else if (start && !ShowScore)
+			ctime <= ctime + 1;
+		else if (!start)
+			ctime <= 0;
+	    
 	end
 	
 	always @ (start or ctime) begin
@@ -34,6 +42,15 @@ module rdysetgo (A, B, C, D, blank, start, clk, reset);
 			D = 4'b1110;
 			blank = 4'b1100;
 		end
+		2'b11: begin
+			
+			A = 4'b0;
+			B = 4'b0;
+			C = 4'b0;
+			D = 4'b0;
+			blank = 4'b0000;
+			ShowScore = 1;
+		end
 		default: begin
 			A = 4'b0;
 			B = 4'b0;
@@ -43,5 +60,67 @@ module rdysetgo (A, B, C, D, blank, start, clk, reset);
 		end
 	endcase
 	end
+	
+	always @ (correct or ShowScore) begin
+		if (ShowScore)
+		begin
+			case(correct) 	
+				2'b01: begin //right
+					if (D >= 9)
+					begin
+						if (C >= 9)
+						begin
+							if (B >= 9)
+							begin
+								if (A >= 9)
+								begin
+									A = 0;
+									B = 0;
+									C = 0;
+									D = 0;
+								end
+								else
+								begin
+									A = A + 1;
+									B = 0;
+									C = 0;
+									D = 0;
+								end
+							end
+							else
+							begin
+								B = B+1;
+								C = 0;
+								D = 0;
+							end
+						end
+						else 
+						begin
+							C = C + 1;
+							D = 0;
+						end
+					end
+					else
+					begin
+						D = D + 1;
+					end
+					
+				end
+				2'b10: begin //wrong
+					//score = 0;
+					ShowScore = 0;
+				end
+				2'b11: begin //time out
+					//finalScore = score;
+					//score = 0;
+					ShowScore = 0;
+					
+				end
+			endcase
+			
+		end
+		
+	end
+	
 	
 endmodule
